91f980e4718adbe593e027be79efec3f0fd6e996
DemonicDemiser <waabaki921642@gmail.com>
2022-04-25 04:22:15 +0600

    Logout function

--- app/src/main/java/com/example/android/gadgetstoreproject/ProfileActivity.java
+++ app/src/main/java/com/example/android/gadgetstoreproject/ProfileActivity.java
@@ -2,13 +2,30 @@ package com.example.android.gadgetstoreproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    private Button logoutButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        logoutButton = findViewById(R.id.logoutButton);
+
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+        });
     }
 }
