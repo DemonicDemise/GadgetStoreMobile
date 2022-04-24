df8ff3d30aa076d75731b8683325e548ffdc6e3b
DemonicDemiser <waabaki921642@gmail.com>
2022-04-25 04:56:44 +0600

    Email, Name and City can be seen in ProfileActivity

--- app/src/main/java/com/example/android/gadgetstoreproject/ProfileActivity.java
+++ app/src/main/java/com/example/android/gadgetstoreproject/ProfileActivity.java
@@ -1,17 +1,30 @@
 package com.example.android.gadgetstoreproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class ProfileActivity extends AppCompatActivity {
 
     private Button logoutButton;
+    private FirebaseUser user;
+    private DatabaseReference reference;
+
+    private String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +38,35 @@ public class ProfileActivity extends AppCompatActivity {
             public void onClick(View view) {
                 FirebaseAuth.getInstance().signOut();
                 startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+        });
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance().getReference("Users");
+        userID = user.getUid();
+
+        final TextView nameTextView = findViewById(R.id.name);
+        final TextView emailTextView = findViewById(R.id.emailAddress);
+        final TextView cityTextView = findViewById(R.id.city);
+
+        reference.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User userProfile = snapshot.getValue(User.class);
+                if(userProfile != null){
+                    String email = userProfile.email;
+                    String name = userProfile.name;
+                    String city = userProfile.city;
+
+                    nameTextView.setText(name);
+                    emailTextView.setText(email);
+                    cityTextView.setText(city);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(getApplicationContext(), "Something went wrong!",Toast.LENGTH_LONG).show();
             }
         });
     }
