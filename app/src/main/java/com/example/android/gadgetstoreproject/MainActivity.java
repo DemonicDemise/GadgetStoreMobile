7d1519ec2074d7413928a4f065c6a9ccbb152ec6
DemonicDemiser <waabaki921642@gmail.com>
2022-04-27 05:47:05 +0600

    Fully covered navigation bar with awesome icons in it

--- app/src/main/java/com/example/android/gadgetstoreproject/MainActivity.java
+++ app/src/main/java/com/example/android/gadgetstoreproject/MainActivity.java
@@ -1,13 +1,16 @@
 package com.example.android.gadgetstoreproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
@@ -15,7 +18,7 @@ import android.widget.ProgressBar;
 import com.example.android.gadgetstoreproject.authentication.LoginActivity;
 import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, NavigationView.OnNavigationItemSelectedListener {
 
     private ProgressBar progressBar;
     private Button signIn;
@@ -28,10 +31,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//
-//        signIn = findViewById(R.id.signInBtn);
-//        signIn.setOnClickListener(this);
 
+        signIn = findViewById(R.id.btnLogin);
+        signIn.setOnClickListener(this);
 
         //Hooks
         drawerLayout = findViewById(R.id.drawer_layout);
@@ -39,18 +41,38 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         toolbar = findViewById(R.id.toolbar);
         //Toolbar
         setSupportActionBar(toolbar);
+        //To make sure that fragments are clickable
+        navigationView.bringToFront();
         //Navigation Drawer Menu
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()){
-//            case R.id.signInBtn:
-//                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
-//                break;
+            case R.id.btnLogin:
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                break;
         }
+    }
+
+    //Closes navigation menu when back button is pressed
+    @Override
+    public void onBackPressed() {
+        //Checking if navigation is open
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }else{
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return true;
     }
 }
